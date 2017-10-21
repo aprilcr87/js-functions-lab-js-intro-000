aa09a10581d96966704d8dbd6d0b99825244b003
April Rivera <aprilcr87@gmail.com>
2017-10-21 17:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function happyHolidays(){
   return 'Happy holidays!'
 }// write your code below!
 
-function happyHolidaysTo(Genya){
-  return "happy holidays, ${}!"
+function happyHolidaysTo(name){
+  var name = 'Genya';
+  return "Happy holidays, ${name}!"
 }
