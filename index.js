e45406f7c886618a7acea081c297fe3b81058b08
April Rivera <aprilcr87@gmail.com>
2017-10-21 17:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function happyHolidays(){
   return 'Happy holidays!'
 }// write your code below!
 
-function happyHolidaysTo(Genya){
-  return `Happy holidays, ${Genya}!`
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
+happyHolidaysTo('Genya');
 
 function happyHolidayTo(Easter, Jesus){
   return `Happy ${Easter}, ${Jesus}`
